957c3c222ac2ea0d9efa0ab7dbafc47bceb12d93
iamshubh22 <sgshubham03@gmail.com>
2017-12-13 17:58:57 +0530

    Minor change

--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 from flask import Flask, send_file
 app = Flask(__name__)
 
-@app.route("/getMap.svg", methods=['GET'])
+
+@app.route("/map", methods=['GET'])
 def getMap():
-    return send_file("map.svg")
+    return send_file("static/map.svg")
+
+@app.route("/image/<imageId>")
+def getImage(imageId):
+    return send_file("static/" + imageId + ".svg")
+
 
 if __name__ == "__main__":
     app.run()
